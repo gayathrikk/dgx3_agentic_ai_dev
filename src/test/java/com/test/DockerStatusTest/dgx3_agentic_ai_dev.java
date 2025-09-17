76ee22fe10bab089a8dddf798298b628b9183255
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-09-17 13:03:51 +0530

    Update dgx3_agentic_ai_dev.java

--- src/test/java/com/test/DockerStatusTest/dgx3_agentic_ai_dev.java
+++ src/test/java/com/test/DockerStatusTest/dgx3_agentic_ai_dev.java
@@ -70,16 +70,13 @@ public class dgx3_agentic_ai_dev {
 
         // TO recipients
         String[] to = {
-            "nitheshkumarsundhar@gmail.com",
-            "ramanan@htic.iitm.ac.in"
+            "sriramv@htic.iitm.ac.in"
         };
 
         // CC recipients
         String[] cc = {
             "divya.d@htic.iitm.ac.in",
-            "venip@htic.iitm.ac.in",
-            "meena@htic.iitm.ac.in",
-            "gayathri@htic.iitm.ac.in"
+            "venip@htic.iitm.ac.in"
         };
 
         String subject = "Docker Container Alert - agentic_ai_dev";
@@ -122,3 +119,4 @@ public class dgx3_agentic_ai_dev {
         }
     }
 }
+
